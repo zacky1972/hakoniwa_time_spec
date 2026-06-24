fefd52a5eeaa6ebc7252a983368a84d28fb02d7a
Susumu Yamazaki <zacky1972@gmail.com>
2026-06-25 06:38:40 +0900

    build: install and set up nstandard

--- mix.exs
+++ mix.exs
@@ -7,7 +7,13 @@ defmodule HakoniwaTimeSpec.MixProject do
       version: "0.1.0",
       elixir: "~> 1.20",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      name: "Hakoniwa time spec",
+      description: "TODO: write a proper description",
+      docs: docs(),
+      package: package(),
+      aliases: aliases(),
+      dialyzer: dialyzer()
     ]
   end
 
@@ -18,10 +24,67 @@ defmodule HakoniwaTimeSpec.MixProject do
     ]
   end
 
+  def docs do
+    [
+      main: "readme",
+      extras: ["README.md"]
+    ]
+  end
+
+  def package do
+    [
+      name: :hakoniwa_time_spec,
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/TODO/hakoniwa_time_spec"}
+    ]
+  end
+
+  def aliases do
+    [
+      check: [
+        "hex.audit",
+        "compile --warnings-as-errors --force",
+        "format --check-formatted --migrate",
+        "credo --strict",
+        "deps.unlock --check-unused",
+        "spellweaver.check",
+        "dialyzer"
+      ],
+      precommit: [
+        "hex.audit",
+        "compile --warnings-as-errors --force",
+        "format --migrate",
+        "credo --strict",
+        "deps.unlock --unused",
+        "spellweaver.check",
+        "dialyzer",
+        "test"
+      ]
+    ]
+  end
+
+  def cli do
+    [
+      preferred_envs: [precommit: :test]
+    ]
+  end
+
+  def dialyzer do
+    [
+      plt_add_apps: [:mix],
+      ignore_warnings: ".dialyzer_ignore.exs"
+    ]
+  end
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:lean_lsp, "~> 0.2.1"}
+      {:nstandard, "~> 0.5", runtime: false},
+      {:lean_lsp, "~> 0.2.1"},
+      {:ex_doc, "~> 0.40", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:spellweaver, "~> 0.1.8", only: [:dev, :test], runtime: false}
     ]
   end
 end
