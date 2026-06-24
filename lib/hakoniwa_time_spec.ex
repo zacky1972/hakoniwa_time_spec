a5316bdc196ee8c207f7a23c9db738a42089b0db
Susumu Yamazaki <zacky1972@gmail.com>
2026-06-25 06:00:28 +0900

    first commit

---
+++ lib/hakoniwa_time_spec.ex
@@ -0,0 +1,18 @@
+defmodule HakoniwaTimeSpec do
+  @moduledoc """
+  Documentation for `HakoniwaTimeSpec`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> HakoniwaTimeSpec.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hakoniwa_time_spec_test.exs
@@ -0,0 +1,8 @@
+defmodule HakoniwaTimeSpecTest do
+  use ExUnit.Case
+  doctest HakoniwaTimeSpec
+
+  test "greets the world" do
+    assert HakoniwaTimeSpec.hello() == :world
+  end
+end
